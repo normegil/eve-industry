1e223d3380e95fe9f80b18ad4de696123070c1d1
Olivier Ory <mail@normegil.be>
2021-02-06 17:36:38 +0100

    Blueprint - Attempt to store blueprint info in DB

--- model/dao/static/blueprints.py
+++ model/dao/static/blueprints.py
@@ -12,16 +12,17 @@ class BlueprintsStatic:
                 blueprint = Blueprint(bp)
                 bp_data = loaded_data[bp]
                 bp_activities = bp_data["activities"]
-                bp_manufacturing = bp_activities["manufacturing"]
-                materials = []
-                for bp_mat in bp_manufacturing["materials"]:
-                    material = Material(bp_mat["typeID"], bp_mat["quantity"])
-                    materials.append(material)
-                products = []
-                for bp_prod in bp_manufacturing["products"]:
-                    product = Product(bp_prod["typeID"], bp_prod["quantity"])
-                    products.append(product)
+                if hasattr(bp_activities, "manufacturing"):
+                    bp_manufacturing = bp_activities["manufacturing"]
+                    materials = []
+                    for bp_mat in bp_manufacturing["materials"]:
+                        material = Material(bp_mat["typeID"], bp_mat["quantity"])
+                        materials.append(material)
+                    products = []
+                    for bp_prod in bp_manufacturing["products"]:
+                        product = Product(bp_prod["typeID"], bp_prod["quantity"])
+                        products.append(product)
 
-                blueprint.manufacturing = Manufacturing(bp_manufacturing["time"], materials, products)
-                blueprints.append(blueprint)
+                    blueprint.manufacturing = Manufacturing(bp_manufacturing["time"], materials, products)
+                    blueprints.append(blueprint)
             return blueprints
